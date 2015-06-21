48a75b9e3aaa5d692e8df50757aa0a4d7674c7df
dandric <Dusan.Andric@gmail.com>
2015-06-20 22:32:04 -0400

    Added extra test cases to make sure generics working fine.

--- src/test/java/powerSetDemo/PoweredHashSetTest.java
+++ src/test/java/powerSetDemo/PoweredHashSetTest.java
@@ -28,7 +28,7 @@ public class PoweredHashSetTest {
 		System.out.println(set.toString());
 		System.out.println(powerSet.getPowerSet().toString());
 		
-		assertEquals("Base Case of Empty Set", powerSet.getPowerSet());
+		assertEquals("Base Case of Empty Set", set, powerSet.getPowerSet());
 	}
 	
 	@Test
@@ -41,6 +41,52 @@ public class PoweredHashSetTest {
 		
 		set.add(new HashSet<Integer>());
 		set.add(s1);
+		
+		System.out.println(set.toString());
+		System.out.println(powerSet.getPowerSet().toString());
+		
+		assertEquals("Base Case of Set of size 1", set, powerSet.getPowerSet());
+	}
+	
+	@Test
+	public void powerSetCheckThree() {
+		List<Integer> intList = Arrays.asList(1,2,3);
+		PoweredHashSet<Integer> powerSet = new PoweredHashSet<Integer>(intList);
+	
+		Set<HashSet<Integer>> set =  new HashSet<HashSet<Integer>>();
+		HashSet<Integer> s1 = new HashSet<Integer>(Arrays.asList(1));
+		HashSet<Integer> s2 = new HashSet<Integer>(Arrays.asList(2));
+		HashSet<Integer> s3 = new HashSet<Integer>(Arrays.asList(3));
+		HashSet<Integer> s12 = new HashSet<Integer>(Arrays.asList(1,2));
+		HashSet<Integer> s13 = new HashSet<Integer>(Arrays.asList(1,3));
+		HashSet<Integer> s23 = new HashSet<Integer>(Arrays.asList(2,3));
+		HashSet<Integer> s123 = new HashSet<Integer>(Arrays.asList(1,2,3));
+		
+		set.add(new HashSet<Integer>());
+		set.addAll(Arrays.asList(s1,s2,s3,s12,s13,s23,s123));
+		
+		System.out.println(set.toString());
+		System.out.println(powerSet.getPowerSet().toString());
+		
+		assertEquals("Base Case of Set of size 1", set, powerSet.getPowerSet());
+	}
+	
+	@Test
+	public void powerSetCheckThreeString() {
+		List<String> stringList = Arrays.asList("x","y","z");
+		PoweredHashSet<String> powerSet = new PoweredHashSet<String>(stringList);
+	
+		Set<HashSet<String>> set =  new HashSet<HashSet<String>>();
+		HashSet<String> s1 = new HashSet<String>(Arrays.asList("x"));
+		HashSet<String> s2 = new HashSet<String>(Arrays.asList("y"));
+		HashSet<String> s3 = new HashSet<String>(Arrays.asList("z"));
+		HashSet<String> s12 = new HashSet<String>(Arrays.asList("x","y"));
+		HashSet<String> s13 = new HashSet<String>(Arrays.asList("x","z"));
+		HashSet<String> s23 = new HashSet<String>(Arrays.asList("y","z"));
+		HashSet<String> s123 = new HashSet<String>(Arrays.asList("x","y","z"));
+		
+		set.add(new HashSet<String>());
+		set.addAll(Arrays.asList(s1,s2,s3,s12,s13,s23,s123));
 		
 		System.out.println(set.toString());
 		System.out.println(powerSet.getPowerSet().toString());
