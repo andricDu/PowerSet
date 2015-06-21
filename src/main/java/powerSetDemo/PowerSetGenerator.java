62b5e3ef1053900b50a493aaf2f562d7b8e177b5
dandric <Dusan.Andric@gmail.com>
2015-06-20 23:07:52 -0400

    Added ability to read file for intput. Arg0: Input File, Arg1: Output File. Assumes CSV.

--- src/main/java/powerSetDemo/PowerSetGenerator.java
+++ src/main/java/powerSetDemo/PowerSetGenerator.java
@@ -3,6 +3,8 @@
  */
 package powerSetDemo;
 
+import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 
 /**
@@ -12,16 +14,45 @@ import java.util.*;
 public class PowerSetGenerator {
 
 	/**
-	 * @param args
+	 * @param args Program Arguments, consisting of input file path, and output file path.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		if (args.length != 2) {
+			System.out.println("Incorrect Number of Arguments");
+			System.out.println("Arg0: Input file");
+			System.out.println("Arg1: Output file");
+			
+		} else {
+			try {
+				String content = new String(Files.readAllBytes(Paths.get(args[0])));
+				System.out.println(content);
+				
+				File file = new File(args[1]);
+				FileOutputStream fop = new FileOutputStream(file);
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				
+				fop.write(getPowerSetFromString(content).toString().getBytes());
+				fop.flush();
+				fop.close();
+				
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
 		
-		List<String> stringList = Arrays.asList("x", "y", "z");
-		PoweredHashSet<String> poweredSet = new PoweredHashSet<String>(stringList);
-		
-		System.out.println(poweredSet.getPowerSet().toString());
-		
+	}
+	
+	/**
+	 * Takes a comma separated list of elements and computes the power set.
+	 * @param args Input String. Assumes comma separated for different elements in set.
+	 * @return Power set of input string. 
+	 */
+	public static PoweredHashSet<PoweredHashSet<String>> getPowerSetFromString(String input) {
+		List<String> stringList = Arrays.asList(input.split(","));
+		PoweredHashSet<String> set = new PoweredHashSet<String>(stringList);
+		return set.getPowerSet();
 	}
 
 }
---
+++ src/test/java/powerSetDemo/PowerSetGeneratorTest.java
@@ -0,0 +1,13 @@
+package powerSetDemo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PowerSetGeneratorTest {
+
+	@Test
+	public void PowerSetFromStringBase() {
+			assertEquals(1,1);	
+	}
+
+}
